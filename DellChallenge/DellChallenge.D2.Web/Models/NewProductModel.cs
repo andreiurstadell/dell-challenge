b52e0c6a78fd28cbdd3a6e73ced08b3647189610
Andrei Ursta <Andrei_Ursta@Dell.com>
2021-04-14 17:50:53 +0300

    wip cleanup

--- DellChallenge/DellChallenge.D2.Web/Models/NewProductModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DellChallenge.D2.Web.Models
-{
-    public class NewProductModel
-    {
-        public string Name { get; set; }
-        public string Category { get; set; }
-    }
-}
--- DellChallenge/DellChallenge.D2.Web/Models/ProductModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DellChallenge.D2.Web.Models
-{
-    public class ProductModel : NewProductModel
-    {
-        public string Id { get; set; }
-    }
-}
